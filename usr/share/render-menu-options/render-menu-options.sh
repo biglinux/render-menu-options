39b3c270ccb474dfe52b455d67f805545215379a
Bruno Gonçalves <bigbruno@gmail.com>
2023-09-10 23:21:58 -0300

    One more test

--- usr/share/render-menu-options/render-menu-options.sh
+++ usr/share/render-menu-options/render-menu-options.sh
@@ -1,52 +1,50 @@
 #!/usr/bin/env bash
 
+# Set Internal Field Separator to newline character to properly read each line of text
 IFS=$'\n'
 
-AddRender() {
-for App in $Applications; do
-	# Select first Exec in .desktop
-	Exec=$(grep -oPm1 "Exec=\K.*" "$App")
-	# Verify if Actions exist
-	if ! grep -q "Actions=" "$App"; then
-		# Add to Actions
-		sed -i "/Actions=/s/$/$AddRender/" "$App"
-	else
-		# Create Actions
-		echo "
-Actions=$AddRender" >> "$App"
-	fi
+# Assign the first argument passed to the script to the variable "Folder"
+Folder=$1
 
-	# Use ; as separator and add Actions based in $AddRender
-	IFS=";"; for RenderMode in $AddRender; do
-		echo "
+# Define a function named AddRenderInFile
+AddRenderInFile() {
+    # Loop over each application listed in the "Applications" variable
+    for App in $Applications; do
+        # Extract the "Exec" command from the .desktop file
+        Exec=$(grep -oPm1 "Exec=\K.*" "$App")
+        
+        # Check if the "Actions" line exists in the .desktop file
+        if grep -q "Actions=" "$App"; then
+            # Append the "AddRender" value to the existing "Actions" line
+            sed -i "/Actions=/s/$/$AddRender/" "$App"
+        else
+            # If "Actions" line doesn't exist, create it and set its value to "AddRender"
+            echo "
+Actions=$AddRender" >> "$App"
+        fi
+
+        # Loop over each rendering mode specified in "AddRender", separated by semicolons
+        IFS=";"; for RenderMode in $AddRender; do
+            # Add a new "[Desktop Action ...]" section for each rendering mode
+            echo "
 [Desktop Action $RenderMode]
 Name=${RenderMode/Render/ Render}
 Exec=$RenderMode $Exec" >> "$App"
-	done
-done
+        done
+    done
 }
 
-if [[ $UUID = 0 ]]; then
-	# If running as root, search in native and flatpak folder
-	Folder='/var/lib/flatpak/exports/share/applications/*.desktop /usr/share/applications/*.desktop'
-else
-	# If running as user, search in home folder
-	Folder="$HOME/.local/share/applications/*.desktop"
-fi
-
-
-# If have optimus-manager Add nvidia, integrated and software options
-# Else only add software option
-# Maybe in future need add amdgpu
+# Check if the "optimus-manager" utility is installed
 if [ -e "/usr/bin/optimus-manager" ]; then
-	# Verify files in native and flatpak folder without NvidiaRender
-	Applications=$(grep -L 'Action NvidiaRender' $Folder)
-	AddRender="NvidiaRender;IntegratedRender;SoftwareRender;" AddRender
+    # Find all .desktop files in the specified folder that do not have an "Action NvidiaRender"
+    Applications=$(grep -L 'Action NvidiaRender' $Folder/*.desktop)
+    AddRender="NvidiaRender;IntegratedRender;SoftwareRender;" AddRenderInFile
 else
-	# Verify files in native and flatpak folder without SoftwareRender
-	Applications=$(grep -L 'Action SoftwareRender' $Folder)
-	AddRender="SoftwareRender;" AddRender
+    # Find all .desktop files in the specified folder that do not have an "Action SoftwareRender"
+    Applications=$(grep -L 'Action SoftwareRender' $Folder/*.desktop)
+    # Define render mode to add as "SoftwareRender;"
+    AddRender="SoftwareRender;" AddRenderInFile
 fi
 
-#Update database of desktop entries
-update-desktop-database
+# Update the desktop database to reflect changes in .desktop files
+update-desktop-database $Folder
